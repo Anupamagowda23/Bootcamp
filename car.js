34c972b4d4160c453d76523c353d265d0ba8784c
Anupamagowda23 <anupamagowda2004@gmail.com>
2024-06-15 10:49:45 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,5 @@
+var audio=document.createElement('audio');
+@import url:('C:\Users\anupa\Downloads\lamborghini-urus-racing-sound-effect-163336.mp3');
+audio.setAttribute("C:\Users\anupa\Downloads\lamborghini-urus-racing-sound-effect-163336.mp3");
+audio.loop=true;
+audio.play();
